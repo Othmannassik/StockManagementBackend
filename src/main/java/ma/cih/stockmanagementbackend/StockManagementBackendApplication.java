e5e6cfb22f61181ca98466b2c4175ecbf4477bf4
Othman Nassik <othmannassik1@gmail.com>
2023-08-10 15:26:13 +0100

    Initial commit

---
+++ src/main/java/ma/cih/stockmanagementbackend/StockManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.cih.stockmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockManagementBackendApplication.class, args);
+    }
+
+}
